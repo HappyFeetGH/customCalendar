fcb94b65e59c0ee7246214f83cb5e3944bf37281
HappyFeetGH <141888688+HappyFeetGH@users.noreply.github.com>
2025-01-21 20:18:19 +0900

    pool update1

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // 연결 제한 설정
+  queueLimit: 0, // 대기열 제한 없음
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Database connection failed:', err.stack);
-    return;
-  }
-  console.log('Connected to database.');
-});
+console.log('Database pool created.');
 
-module.exports = connection;
+
+module.exports = {
+  pool,
+  promisePool: pool.promise(), // Promise 기반과 기존 방식을 모두 지원
+};
