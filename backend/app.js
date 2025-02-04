a86aaa7ca654ef9aac48d60a20730056e329bff0
HappyFeetGH <141888688+HappyFeetGH@users.noreply.github.com>
2025-02-04 17:30:49 +0900

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
+
 
 const API_BASE_URL = "http://144.24.69.30";
 
@@ -19,6 +21,8 @@ app.use(express.json());
 const eventRoutes = require('./routes/events');
 app.use('/api/events', eventRoutes);
 
+const mergeRoutes = require('./routes/purchase');
+app.use('/api/purchase', mergeRoutes);
 
 // 서버 시작
 const PORT = process.env.PORT || 5000;
